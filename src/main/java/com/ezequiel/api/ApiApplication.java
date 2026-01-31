bb1771a8ee9c6dd2f5b3b5d297e2e6138b007513
Ezequiel <ezequielsilvalima01@gmail.com>
2026-01-31 20:03:11 -0300

    commit

--- src/main/java/com/ezequiel/api/ApiApplication.java
+++ src/main/java/com/ezequiel/api/ApiApplication.java
@@ -1,13 +1,23 @@
 package com.ezequiel.api;
 
+import com.ezequiel.api.service.ApiService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	private final ApiService apiService;
+
+	public ApiApplication(ApiService apiService) {
+		this.apiService = apiService;
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(Math.class, args);
 	}
 
+	@Override
+	public void run(String... args){
+		apiService.getEndereco();
+	}
 }
--- src/main/java/com/ezequiel/api/service/ApiService.java
+++ src/main/java/com/ezequiel/api/service/ApiService.java
@@ -1,12 +1,8 @@
 package com.ezequiel.api.service;
 
 import com.ezequiel.api.dto.EnderecoDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,26 +11,22 @@ import java.net.http.HttpResponse;
 @Service
 public class ApiService {
 
-    public EnderecoDto getEndereco(String cep){
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://restcountries.com/v3.1/independent?status=true")).build();
-        try {
+    EnderecoDto enderecoDto = new EnderecoDto();
+
+    public EnderecoDto getEndereco(){
+        try{
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://restcountries.com/v3.1/independent?status=true")).build();
+
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+
+
+            ObjectMapper mapper = new ObjectMapper();
+            enderecoDto = mapper.readValue(response.body(), EnderecoDto.class);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+        return enderecoDto;
     }
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    public String allDates(){
-        String url = "https://restcountries.com/v3.1/independent?status=true";
-
-        ResponseEntity<String> forEntity = restTemplate.getForEntity(url, String.class);
-
-        return forEntity.getBody();
-    }
-
 }
